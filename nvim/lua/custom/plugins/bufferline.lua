e42b386519700756420b7fff2d831de1a5ae03ec
Andre Viallon <avi@trackunit.com>
2025-04-10 13:34:28 +0200

    Fix tree offset

--- nvim/lua/custom/plugins/bufferline.lua
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -27,6 +27,12 @@ return {
       offsets = {
         {
           filetype = 'neo-tree',
+          text = 'Neo-tree',
+          highlight = 'Directory',
+          text_align = 'left',
+        },
+        {
+          filetype = 'snacks_layout_box',
         },
       },
       max_name_length = 100,
